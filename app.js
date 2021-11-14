13794e17765678844e9683c665eca649dcd89385
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-11-14 21:36:40 +0200

    other commit

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-let btnShow=document.querySelector(".btnShow");
+let btn=document.querySelector(".btnShow")
 let container=document.querySelector(".container");
+
+
+let flag=0;
+
+btn=addEventListener("click",()=>{
+
+   
+    if(flag==0){
+      
+         container.style.display="none";
+         flag=1;
+    }else{
+        container.style.display="block";
+        flag=0;
+    }
+   
+
+
+})
+
+
 
 
