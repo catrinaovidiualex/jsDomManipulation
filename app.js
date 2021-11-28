7d87fa1af3495a6c7e6bbe8e96e4b2bd72e1a298
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-11-28 18:43:05 +0200

    other commit

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ let newitem=document.querySelector(".newitemclass");
 let btnRemoveItem=document.querySelector(".btnDelete");
 let btnCH=document.querySelector(".btnChange");
 
+
+
 let flag=1;
 
 btnSH.addEventListener("click",()=>{
@@ -38,19 +40,48 @@ btnCH.addEventListener("click",()=>{
 
 btnAdaugare.addEventListener("click",()=>{
     let elem=document.createElement("li");
-
+    
     elem.textContent=newitem.value;
+
+    attachbtn(elem);
+
     lista.append(elem);
 
+    
     newitem.value="";
     
 
 })
 
-btnRemoveItem.addEventListener("click",()=>{
-      
-    lista.removeChild(lista.lastChild);
-})
+
+
+function attachbtn(li){
+
+    li.classList.add("purple");
+    let spanitem1=document.createElement('span');
+    spanitem1.classList.add('up');
+    spanitem1.classList.add('btns');
+    spanitem1.textContent="Up";
+    li.appendChild(spanitem1);
+
+
+    li.classList.add("purple");
+    let spanitem2=document.createElement('span');
+    spanitem2.classList.add('dw');
+    spanitem2.classList.add('btns');
+    spanitem2.textContent="Down";
+    li.appendChild(spanitem2);
+
+
+    li.classList.add("purple");
+    let spanitem3=document.createElement('span');
+    spanitem3.classList.add('dlt');
+    spanitem3.classList.add('btns');
+    spanitem3.textContent="Delete";
+    li.appendChild(spanitem3);
+
+
+}
 
 
 
